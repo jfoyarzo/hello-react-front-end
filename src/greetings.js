fd0590c73072601def1249eca75953242c15787b
jfoyarzo <foyarzo621@gmail.com>
2023-03-07 22:23:33 -0300

    Add greeting component

---
+++ src/greetings.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreetings } from './redux/greeting/greeting_slice';
+
+const Greetings = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, [dispatch]);
+
+  const greetings = useSelector((state) => state.greetings);
+
+  return (<h1>{greetings.text}</h1>);
+};
+
+export default Greetings;
