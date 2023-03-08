88e190e56e0810f139fcdbe9dc85cfb583fb51bc
jfoyarzo <foyarzo621@gmail.com>
2023-03-08 17:14:48 -0300

    Remove comments from redux files

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { configureStore } from '@reduxjs/toolkit';
 import greetingsReducer from './greeting/greeting_slice';
 
--- src/redux/greeting/greeting_slice.js
+++ src/redux/greeting/greeting_slice.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = [];
